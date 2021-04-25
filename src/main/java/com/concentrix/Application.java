7ad7d51917b3b4ad6905461a9cf218d541a63dd5
Shehbaz PC <shehbazadhal@gmail.com>
2021-04-25 12:22:19 +0530

    HIS-134

--- src/main/java/com/concentrix/Application.java
+++ src/main/java/com/concentrix/Application.java
@@ -9,6 +9,8 @@ public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 		System.out.println("Test");
+		//Changes for HIS-134
+		System.out.println("134 Fixed");
 	}
 
 }
